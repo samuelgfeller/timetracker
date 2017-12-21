8df53a46be74510a3c639d52845f7834f953d83c
Samuel Gfeller <sgfeller@wmc.ch>
2017-12-21 17:11:27 +0100

    added Service

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,58 @@ var $ =require('jquery');
     });
     $("#showResultsBtn").css('display',"none");
 }*/
-/*Delete*/
+$( document ).ready(function(){
+
+
+// Get the modal
+    var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            $('.modal-body').html("<br>Loading...<br><br>");
+        }
+    };
+    closeModal=function(){
+        document.getElementById('myModal').style.display = "none";
+        $('.modal-body').html("<br>Loading...<br><br>");
+
+    };
+    display=function (url) {
+        $.ajax({
+            url: url,
+            type: 'post',
+            success: function (output) {
+                $('.modal-body').html(output);
+            }
+        });
+
+        modal.style.display = "block";
+    };
+
+
+    /*Delete*/
+    delOrt=function(url,id,toDel){
+        $.ajax({ url: url,
+            type: 'post',
+            data: {
+                "id": id
+            },
+            success: function(output) {
+                if(toDel==='ort'){
+                    $('#ort'+id).remove();
+                }else if(toDel==='company'){
+                    $('#company'+id).remove();
+                }
+                // window.location.replace("http://localhost:8000/timetracker/orte");
+            }
+        });
+
+    }
+
+});
 
 
